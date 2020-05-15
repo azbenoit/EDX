afae734de848150d8403acb2219d66d8c70eb12c
Alix Benoit <alixb1908@gmail.com>
2020-05-14 22:00:02 -0400

    Committing 2 new linreg scripts and modifying one

--- linear_regression/baseball_lr.R
+++ linear_regression/baseball_lr.R
@@ -1,7 +1,10 @@
 library(Lahman)
 data("Teams")
+?Teams
 
-Teams %>% filter(yearID >= 1961, yearID <= 2001) %>% 
-  mutate(win_rate = W/(W+L)) %>% 
+teams <- Teams %>% filter(yearID >= 1961, yearID <= 2001) %>% 
+  mutate(win_rate = W/(W+L), R_avg = R/G, AB_avg = AB/G, E_avg = E/G)
+plot <- teams %>%
   ggplot(aes(X2B, X3B)) +
   geom_point(alpha = .5)
+teams %>% summarise(RABcor = cor(X2B/G,X3B/G)) %>% .$RABcor
---
+++ linear_regression/bivariate_normal.R
@@ -0,0 +1,21 @@
+# create the dataset
+library(tidyverse)
+library(HistData)
+data("GaltonFamilies")
+set.seed(1983)
+galton_heights <- GaltonFamilies %>%
+  filter(gender == "male") %>%
+  group_by(family) %>%
+  sample_n(1) %>%
+  ungroup() %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+
+
+# Making individual qqplots for each strata of Zscores in order to check if this data is bivariate normal
+galton_heights %>%
+  mutate(z_father = round((father - mean(father)) / sd(father))) %>%
+  filter(z_father %in% -2:2) %>%
+  ggplot() +  
+  stat_qq(aes(sample = son)) +
+  facet_wrap( ~ z_father)
